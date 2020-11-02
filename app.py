ac4d1967dd93a33e5584485f8a1ec40ae0f3f106
JohnnyYamanaka <yamanaka.johnny@outlook.com>
2020-11-02 09:39:06 -0300

    criando crud

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, url_for, redirect, request
 import datetime
 from models import Task
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
 
 lista_task = []
 
--- models.py
+++ models.py
@@ -1,6 +1,18 @@
+from app import db
 
 
-class Task:
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    desc_curta = db.Column(db.String(30), unique=True, nullable=False)
+    desc_longa = db.Column(db.String(99))
+    status = db.Column(db.Boolean)
+    prioridade = db.Column(db.String(10), nullable=False)
+    data_inicio = db.Column(db.DateTime)
+    data_conclusao = db.Column(db.DateTime)
+
+
     def __init__(self, desc_curta, desc_longa, status, prioridade,
     data_inicio, data_conclusao, id=None):
         self.id = id
@@ -10,3 +22,6 @@ class Task:
         self.prioridade = prioridade
         self.data_inicio = data_inicio
         self.data_conclusao = data_conclusao
+
+        def __repr__(self):
+            return '<desc_curta %r>' % self.desc_curta
